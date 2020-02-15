0d4028d64e749aeefe8da726e5b4ff464a65bfca
Chris Staunton <stauntch@tcd.ie>
2020-02-15 20:30:07 +0000

    added insertion, selection and quick sort

---
+++ SortComparison.java
@@ -0,0 +1,165 @@
+// -------------------------------------------------------------------------
+
+/**
+ * This class contains static methods that implementing sorting of an array of
+ * numbers using different sort algorithms.
+ *
+ * @author
+ * @version HT 2020
+ */
+
+class SortComparison {
+
+	/**
+	 * Sorts an array of doubles using InsertionSort. This method is static, thus it
+	 * can be called as SortComparison.sort(a)
+	 * 
+	 * @param a: An unsorted array of doubles.
+	 * @return array sorted in ascending order.
+	 *
+	 */
+	static double[] insertionSort(double a[]) {
+
+		// todo: implement the sort
+
+		for (int i = 1; i < a.length; i++) {
+			double tmp = a[i];
+			int j = i;
+
+			while (j > 0 && tmp < a[j - 1]) {
+				a[j] = a[j - 1];
+				j = j - 1;
+			}
+			a[j] = tmp;
+		}
+		return a;
+	}// end insertionsort
+
+	/**
+	 * Sorts an array of doubles using Selection Sort. This method is static, thus
+	 * it can be called as SortComparison.sort(a)
+	 * 
+	 * @param a: An unsorted array of doubles.
+	 * @return array sorted in ascending order
+	 *
+	 */
+	static double[] selectionSort(double a[]) {
+
+		// todo: implement the sort
+		for (int i = 0; i < a.length; i++) {
+			int minIndex = i;
+			// int j ;
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[j] < a[minIndex]) {
+					minIndex = j;
+				}
+
+			}
+			// swap
+			double tmp = a[minIndex];
+			a[minIndex] = a[i];
+			a[i] = tmp;
+		}
+		return a;
+	}// end selectionsort
+
+	/**
+	 * Sorts an array of doubles using Quick Sort. This method is static, thus it
+	 * can be called as SortComparison.sort(a)
+	 * 
+	 * @param a: An unsorted array of doubles.
+	 * @return array sorted in ascending order
+	 *
+	 */
+
+	private static int partition(double a[], int low, int hi) {
+		int i = low;
+		int j = hi + 1;
+		double pivot = a[low];
+		while (true) {
+			while ((a[++i] < pivot)) {
+				if (i == hi)
+					break;
+			}
+			while (((a[--j]) < pivot)) {
+				if (j == low)
+					break;
+			}
+			if (i >= j)
+				break;
+			double temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+		}
+		a[low] = a[j];
+		a[j] = pivot;
+		return j;
+
+	}
+
+	public void sort(double[] a) {
+		recursiveQuick(a, 0, a.length - 1);
+	}
+
+	static double[] recursiveQuick(double[] numbers, int lo, int hi) {
+		if (hi <= lo) {
+			return numbers;
+		}
+		int pivotPos = partition(numbers, lo, hi);
+		recursiveQuick(numbers, lo, pivotPos - 1);
+		recursiveQuick(numbers, pivotPos + 1, hi);
+		return numbers;
+	}
+
+//	static double[] quickSort(double a[]) {
+//		if ((a.length <= 1)) {
+//			return a;
+//		}
+//
+//		// todo: implement the sort
+//
+//	}// end quicksort
+
+	/**
+	 * Sorts an array of doubles using Merge Sort. This method is static, thus it
+	 * can be called as SortComparison.sort(a)
+	 * 
+	 * @param a: An unsorted array of doubles.
+	 * @return array sorted in ascending order
+	 *
+	 */
+	/**
+	 * Sorts an array of doubles using iterative implementation of Merge Sort. This
+	 * method is static, thus it can be called as SortComparison.sort(a)
+	 *
+	 * @param a: An unsorted array of doubles.
+	 * @return after the method returns, the array must be in ascending sorted
+	 *         order.
+	 */
+
+	static double[] mergeSortIterative(double a[]) {
+
+		// todo: implement the sort
+
+	}// end mergesortIterative
+
+	/**
+	 * Sorts an array of doubles using recursive implementation of Merge Sort. This
+	 * method is static, thus it can be called as SortComparison.sort(a)
+	 *
+	 * @param a: An unsorted array of doubles.
+	 * @return after the method returns, the array must be in ascending sorted
+	 *         order.
+	 */
+	static double[] mergeSortRecursive(double a[]) {
+
+		// todo: implement the sort
+
+	}// end mergeSortRecursive
+
+	public static void main(String[] args) {
+
+		// todo: do experiments as per assignment instructions
+	}
+
+}// end class
---
+++ SortComparisonTest.java
@@ -0,0 +1,76 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+//-------------------------------------------------------------------------
+/**
+ *  Test class for SortComparison.java
+ *
+ *  @author
+ *  @version HT 2020
+ */
+@RunWith(JUnit4.class)
+public class SortComparisonTest
+{
+    //~ Constructor ........................................................
+    @Test
+    public void testConstructor()
+    {
+        new SortComparison();
+    }
+
+    //~ Public Methods ........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Check that the methods work for empty arrays
+     */
+    @Test
+    public void testEmpty()
+    {
+    }
+    
+    @Test
+    public void testInsertionSort() {
+    	//SortComparison sorter = new SortComparison();
+    	double a[] = {12, 11, 13, 5, 6};
+    	double b[] = {5.0, 6.0, 11.0, 12.0, 13.0};
+    	assertEquals(Arrays.toString(b), Arrays.toString(SortComparison.insertionSort(a)));
+    }
+    
+    @Test
+    public void testSelectionSort() {
+    	//SortComparison sorter = new SortComparison();
+    	double a[] = {12, 11, 13, 5, 6};
+    	double b[] = {5.0, 6.0, 11.0, 12.0, 13.0};
+    	assertEquals(Arrays.toString(b), Arrays.toString(SortComparison.selectionSort(a)));
+    }
+    
+    @Test
+    public void testQuickSort() {
+    	//SortComparison sorter = new SortComparison();
+    	double a[] = {12, 11, 13, 5, 6};
+    	double b[] = {5.0, 6.0, 11.0, 12.0, 13.0};
+    	assertEquals(Arrays.toString(b), Arrays.toString(SortComparison.selectionSort(a)));
+    }
+
+
+    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
+    // be executed at least once from at least one test.
+
+    // ----------------------------------------------------------
+    /**
+     *  Main Method.
+     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
+     *
+     */
+    public static void main(String[] args)
+    {
+        //TODO: implement this method
+    }
+
+}
